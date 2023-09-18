d0831b777b9352eda6f4beb5569d991db5011474
WalterGuerra <wsteven00@gmail.com>
2023-09-18 17:48:50 -0400

    add form for new flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,11 @@
 import { Flight } from "../models/flight.js";
 
-
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: "New Flight"
+  })
+}
 
 export {
-  
+  newFlight as new
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,8 +4,6 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 // GET localhost:3000/users
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+router.get('/new', flightsCtrl.new)
 
 export { router }
