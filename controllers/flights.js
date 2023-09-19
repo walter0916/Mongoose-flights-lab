f6501f86a5f99862036ece858aebb3e7cb4bacc4
WalterGuerra <wsteven00@gmail.com>
2023-09-19 12:30:00 -0400

    add create functionality for new flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -6,6 +6,21 @@ function newFlight(req, res) {
   })
 }
 
+function create(req, res){
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  Flight.create(req.body)
+  .then(flights => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
 export {
-  newFlight as new
+  newFlight as new,
+  create
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,5 +5,6 @@ const router = Router()
 
 // GET localhost:3000/users
 router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
 
 export { router }
